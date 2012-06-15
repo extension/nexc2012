d3cb08b41dbdab988549ea921e9e3338072650f6
Ivelin Denev <idengo@gmail.com>
2012-06-15 18:10:53 +0300

    removing line brakes from google analytics code

--- wp-content/themes/spuron/footer.php
+++ wp-content/themes/spuron/footer.php
@@ -16,18 +16,16 @@
 <?php wp_footer(); ?>
 <script type="text/javascript">
 
- var _gaq = _gaq || [];
- _gaq.push(['_setAccount', 'UA-155321-47']);
- _gaq.push(['_trackPageview']);
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-155321-47']);
+  _gaq.push(['_setDomainName', 'extension.org']);
+  _gaq.push(['_trackPageview']);
 
- (function() {
-   var ga = document.createElement('script'); ga.type =
-'text/javascript'; ga.async = true;
-   ga.src = ('https:' == document.location.protocol ? 'https://ssl' :
-'http://www') + '.google-analytics.com/ga.js';
-   var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(ga, s);
- })();
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 
 </script>
 </body>
